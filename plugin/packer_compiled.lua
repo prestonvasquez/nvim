1174c5f3cc49df3a33d06caa8c864c2cdfdb30c8
Preston Vasquez <24281431+prestonvasquez@users.noreply.github.com>
2023-04-18 19:25:03 -0600

    add markdown support, update theme

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -99,6 +99,13 @@ _G.packer_plugins = {
     path = "/Users/prestonvasquez/.local/share/nvim/site/pack/packer/start/github-nvim-theme",
     url = "https://github.com/projekt0n/github-nvim-theme"
   },
+  ["markdown-preview.nvim"] = {
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/prestonvasquez/.local/share/nvim/site/pack/packer/opt/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
+  },
   nerdtree = {
     loaded = true,
     path = "/Users/prestonvasquez/.local/share/nvim/site/pack/packer/start/nerdtree",
@@ -134,11 +141,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/prestonvasquez/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
-  },
-  purify = {
-    loaded = true,
-    path = "/Users/prestonvasquez/.local/share/nvim/site/pack/packer/start/purify",
-    url = "https://github.com/kyoz/purify"
   },
   ["rust.vim"] = {
     loaded = true,
@@ -208,10 +210,21 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Setup for: markdown-preview.nvim
+time([[Setup for markdown-preview.nvim]], true)
+try_loadstring("\27LJ\2\n=\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\2\0\0\rmarkdown\19mkdp_filetypes\6g\bvim\0", "setup", "markdown-preview.nvim")
+time([[Setup for markdown-preview.nvim]], false)
 -- Config for: octo.nvim
 time([[Config for octo.nvim]], true)
 try_loadstring("\27LJ\2\n2\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\tocto\frequire\0", "config", "octo.nvim")
 time([[Config for octo.nvim]], false)
+vim.cmd [[augroup packer_load_aucmds]]
+vim.cmd [[au!]]
+  -- Filetype lazy-loads
+time([[Defining lazy-load filetype autocommands]], true)
+vim.cmd [[au FileType markdown ++once lua require("packer.load")({'markdown-preview.nvim'}, { ft = "markdown" }, _G.packer_plugins)]]
+time([[Defining lazy-load filetype autocommands]], false)
+vim.cmd("augroup END")
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
