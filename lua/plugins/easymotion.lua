eff71c535c43d94b6f9e70392ae40e084aec86a4
Preston Vasquez <prestonvasquez@icloud.com>
2025-01-31 15:37:44 -0700

    Update for lazymotion

--- lua/plugins/easymotion.lua
+++ lua/plugins/easymotion.lua
@@ -1,3 +1,10 @@
 return {
   "easymotion/vim-easymotion",
+  config = function()
+    -- additional configuration can be done here
+  end,
+  keys = {
+    -- Remap <leader><space> to EasyMotion
+    { "<leader><space>", "<Plug>(easymotion-bd-jk)" },
+  },
 }
